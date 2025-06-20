f08563233e09bcf20a2e37eb6bdf7d56bec277af
Kolli Lokesh Reddy <kollilokeshreddy18@gmail.com>
2025-06-20 19:38:33 +0530

    Update cors origin

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const app = express();
 // Middleware to handle CORS
 app.use(
   cors({
-    origin: "https://ace-bot-ai.vercel.app/",
+    origin: "https://ace-bot-ai.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
